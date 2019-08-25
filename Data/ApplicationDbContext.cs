04d9b1f934a05889dcd122df22c76fdee25702d9
Jimmy <jimmymetts@bellsouth.net>
2019-08-25 13:49:03 -0500

    Update Books and Authors on nav

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Bookshelf2.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookshelf2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public  DbSet<ApplicationUser> ApplicationUser { get; set; }
+        public  DbSet<Author> Author { get; set; }
+        public  DbSet<Book> Book { get; set; }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookshelf2.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+        public string PenName { get; set; }
+
+        public string PreferredGenre { get; set; }
+
+        public List<Book> Books   { get; set; }
+        public string ApplicationUserId { get; set; }
+    }
+}
